b1cb1dee199574841da132da5c44987b232ee792
Marek Kubica <marek@tarides.com>
2025-09-09 10:26:15 +0200

    Start of Gosu grammar

--- grammar.js
+++ grammar.js
@@ -11,7 +11,18 @@ module.exports = grammar({
   name: "gosu",
 
   rules: {
+    // TODO recurse here
+    id: $ => seq("a"),
+
+    dot_path_word: $ => seq(id, repeat(".", id)),
+    uses_statement: $ => seq(dot_path_word, optional(".*"), optional(repeat(";"))),
+    uses_statement_list: $ => seq("uses", uses_statement, repeat(seq("uses", uses_statement))),
+    header: $ => seq(optional(seq("package")), uses_statement_list),
+
+    // TODO: rest of start
+    start: $ => header,
+
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => start
   }
 });
